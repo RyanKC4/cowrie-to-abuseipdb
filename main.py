add7750f5f9acd4279fb180d3ff8f3e2c268516d
Ryan <ryankc06@proton.me>
2026-05-09 19:25:31 -0500

    Added attempt count to report_aggregator

--- main.py
+++ main.py
@@ -10,10 +10,17 @@ def report_aggregator(jsonFile):
             if entryItem['eventid'] == 'cowrie.login.failed' or entryItem['eventid'] == 'cowrie.login.success':
                 if entryItem['src_ip'] not in data:
                     data[entryItem['src_ip']] = [1,entryItem['timestamp']]
+                else:
+                    data[entryItem['src_ip']][0] += 1
     return data
+
+#def report_compiler(data):
+
 
 def main():
     data = report_aggregator(argv[1])
+    for i,j in data.items():
+        print(i,j)
 
 if __name__ == '__main__':
     main()
